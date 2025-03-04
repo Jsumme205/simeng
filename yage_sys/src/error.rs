d5ed39522524055eec1a3f52ab6579792b85633c
Jsumme205 <119705812+Jsumme205@users.noreply.github.com>
2025-03-04 10:59:15 -0800

    more docs

--- yage_sys/src/error.rs
+++ yage_sys/src/error.rs
@@ -1,4 +1,7 @@
+// TODO: add error implementation for #[cfg(feature = "std")]
+
 use core::fmt::Debug;
+
 
 #[cfg(not(feature = "std"))]
 use alloc::boxed;
@@ -6,22 +9,33 @@ use alloc::boxed;
 #[cfg(feature = "std")]
 use std::boxed;
 
+
+/// the kind of error that occured, 
+/// this is non-exhaustive, and subject to change in the future
 #[non_exhaustive]
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub enum ErrorKind {
+    /// we failed to initialize the GLFw Instance
     FailedInit,
+    /// the call to `glfwCreateWindow` returned NULL
     WindowNull,
+    /// we failed getting the dimensions from a window
     FailedGettingDims,
+    /// other error
     Other,
 }
 
+/// an error that occured in GLFW
 pub struct GlfwError {
+    // the kind of error
     kind: ErrorKind,
     //#[cfg(feature = "")]
+    
     payload: Option<boxed::Box<dyn AsRef<str>>>,
 }
 
 impl GlfwError {
+    /// creates a simple error, with just a `ErrorKind`
     pub const fn simple(kind: ErrorKind) -> Self {
         Self {
             kind,
@@ -30,6 +44,7 @@ impl GlfwError {
         }
     }
 
+    /// creates a error with a kind and a payload, which implements `AsRef<str>`
     //#[cfg(feature = "alloc")]
     pub fn with_payload<P>(kind: ErrorKind, payload: P) -> Self
     where
@@ -41,6 +56,7 @@ impl GlfwError {
         }
     }
 
+    /// returns the kind of the error.
     pub const fn kind(&self) -> ErrorKind {
         self.kind
     }
