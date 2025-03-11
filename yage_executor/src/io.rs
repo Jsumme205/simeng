7ec09648ee993f9cd308c351f32bee4f89f15b13
Jsumme205 <119705812+Jsumme205@users.noreply.github.com>
2025-03-11 09:49:47 -0700

    yeehaw my beets

--- yage_executor/src/io.rs
+++ yage_executor/src/io.rs
@@ -1,1 +1,37 @@
+use yage_util::{list::{Link, LinkedList, Pointers}, atomic::Atomic};
+use yage_net::Interest;
+use std::cell::UnsafeCell;
+use std::marker::PhantomPinned;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::task::{Waker, Context};
+use std::ptr::NonNull;
+
+pub(super) struct Io {
+    pointers: UnsafeCell<Pointers<Self>>,
+    readiness: AtomicUsize,
+    waiters: Atomic<Waiters>
+}
+
+struct Waiters {
+  list: LinkedList<Waiter>
+  reader: Option<Waker>,
+  writer: Option<Waker>
+}
+
+struct Waiter {
+  pointers: Pointers<Self>,
+  waker: Option<Waker>,
+  interests: Interest,
+  ready: bool,
+  _pin: PhantomPinned,
+}
+
+
+impl Waiter {
+  unsafe fn address_of_pointers(this: NonNull<Self>) -> NonNull<Pointers<Self> {
+    unsafe {
+      NonNull::new_unchecked(&raw mut (*this.as_ptr()).pointers)
+    }
+  }
+}
 
