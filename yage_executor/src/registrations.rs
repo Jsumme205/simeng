78159e4eef6523c5f55809351cd5dcd3d4e55049
Jsumme205 <119705812+Jsumme205@users.noreply.github.com>
2025-03-11 08:29:15 -0700

    Update registrations.rs

--- yage_executor/src/registrations.rs
+++ yage_executor/src/registrations.rs
@@ -1,1 +1,13 @@
 
+use std::sync::{Mutex, Arc, atomic::{AtomicUsize, Ordering}};
+use yage_util::list::{Link, LinkedList};
+
+pub(super) struct Registrations {
+  num_pending_release: AtomicUsize
+}
+
+pub(super) struct Synced {
+  shutdown: bool,
+  registrations: LinkedList<Arc<Io>>
+  pending_drop: SmallVec<Arc<Io>>
+}
